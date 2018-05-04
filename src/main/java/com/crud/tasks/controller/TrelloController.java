13b55194f22bbffc57cc5c1821ad1ec9e73850a3
PStopa <pawel.stopa.92@gmail.com>
2018-05-04 18:56:29 +0200

    TrelloBoards Optional response

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,33 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/v1/trello")
+public class TrelloController {
+
+    @Autowired
+    private TrelloClient trelloClient;
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
+    public void getTrelloBoards(){
+
+            Optional<List<TrelloBoardDto>> trelloBoards = trelloClient.getTrelloBoards();
+
+            if(trelloBoards.isPresent()){
+                trelloBoards.get().stream()
+                        .filter(boards -> (boards.getId() != null && boards.getName().contains("Kodilla")))
+                        .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+            } else {
+                System.out.println("Trello Boards is empty");
+            }
+    }
+}
