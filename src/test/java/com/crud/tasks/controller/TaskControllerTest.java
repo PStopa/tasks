f5442d74fbf75ca2905631081968482be008183c
PStopa <pawel.stopa.92@gmail.com>
2018-06-14 13:32:15 +0200

    Test refactoring

--- src/test/java/com/crud/tasks/controller/TaskControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -54,7 +54,7 @@ public class TaskControllerTest {
         when(taskController.getTasks()).thenReturn(taskDtoList);
 
         //When & Then
-        mockMvc.perform(get("/v1/task/getTasks").contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/v1/tasks").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$",hasSize(1)))
                 .andExpect(jsonPath("$[0].id", is(1)))
@@ -71,7 +71,7 @@ public class TaskControllerTest {
         when(taskController.getTask(1L)).thenReturn(taskDto);
 
         //When & Then
-        mockMvc.perform(get("/v1/task/getTask?taskId=" + 1L).contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/v1/tasks/" + 1L).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", is(1)))
                 .andExpect(jsonPath("$.title", is("Title")))
@@ -84,7 +84,7 @@ public class TaskControllerTest {
         Task task = new Task(1L, "Title", "Content");
         service.saveTask(task);
         // When & Then
-        mockMvc.perform(delete("/v1/task/deleteTask?taskId=" + 1).contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(delete("/v1/tasks/" + 1L).contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk());
     }
 
@@ -102,7 +102,7 @@ public class TaskControllerTest {
         Gson gson = new Gson();
         String jsonContent = gson.toJson(taskDto);
         //When & Then
-        mockMvc.perform(put("/v1/task/updateTask").contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8").content(jsonContent))
+        mockMvc.perform(put("/v1/tasks/").contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8").content(jsonContent))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", is(1)))
                 .andExpect(jsonPath("$.title", is("Title")))
@@ -122,7 +122,7 @@ public class TaskControllerTest {
         Gson gson = new Gson();
         String jsonContent = gson.toJson(taskDto);
 
-        mockMvc.perform(post("/v1/task/createTask").contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8").content(jsonContent))
+        mockMvc.perform(post("/v1/tasks/").contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8").content(jsonContent))
                 .andExpect(status().isOk());
     }
 }
--- src/test/java/com/crud/tasks/trello/config/TrelloConfigTest.java
+++ src/test/java/com/crud/tasks/trello/config/TrelloConfigTest.java
@@ -2,6 +2,8 @@ package com.crud.tasks.trello.config;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
